146e928bfde914ede9429bb570a0c4ee272a7322
Александр Хрусталёв <st.alex.ser@gmail.com>
2019-01-11 22:16:13 +0200

    обновил галп 3 на галп 4 и синтаксис галп файла

--- gulpfile-b.js
+++ gulpfile-b.js
@@ -17,9 +17,9 @@ gulp.task('sass', function () { // Создаем таск Sass
   return combiner(
     gulp.src('sass/**/*.sass'),// Берем источник
     sass(), // Преобразуем Sass в CSS посредством gulp-sass
-    autoprefixer(['last 15 versions', '> 1%', 'ie 9'], {cascade: true}), // Создаем префиксы
+    autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true}), // Создаем префиксы
     gulp.dest('css'), // Выгружаем результата в папку src/css
-    browserSync.reload({stream: true}),
+    browserSync.reload({stream: true})
   ).on('error', notify.onError()); // Обновляем CSS на странице при изменении, и в случае ошибки выводим оповещение.
 });
 
@@ -35,11 +35,13 @@ gulp.task('browser-sync', function () { // Создаем таск browser-sync
 gulp.task('scripts', function () {
   return gulp.src([ // Берем все необходимые библиотеки
     'libs/jquery/dist/jquery.min.js', // Берем jQuery
-    // 'libs/Easy-Customizable-Sliding-Menu-Indicator-Plugin-SlidingMenu/SlidingMenu.js',
-    // 'libs/TweenMax/TweenMax.min.js',
+    // '',
+    'libs/gsap/src/minified/TweenMax.min.js',
+    
+    // 'libs/fullpage.js/jquery.slimscroll.min.js',
+    // 'libs/fullpage.js/jquery.fullPage.min.js',
     // 'libs/slick/slick.min.js',
     // 'libs/magnific-popup/dist/jquery.magnific-popup.min.js',
-    'libs/gsap/src/minified/TweenMax.min.js',
     'libs/wow/dist/wow.min.js'
   ])
     .pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
@@ -47,10 +49,9 @@ gulp.task('scripts', function () {
     .pipe(gulp.dest('js')); // Выгружаем в папку src/js
 });
 
-gulp.task('css-libs', gulp.parallel('sass'), function () {
+gulp.task('css-libs', ['sass'], function () {
   return gulp.src([ // Выбираем файл для минификации
     'libs/normalize.css/normalize.css',
-    // 'css/main.css',
     // 'libs/fullpage.js/dist/fullpage.min.css',
     // 'libs/magnific-popup/dist/magnific-popup.css',
     // 'libs/slick/slick.css',
@@ -62,19 +63,9 @@ gulp.task('css-libs', gulp.parallel('sass'), function () {
     .pipe(gulp.dest('css')); // Выгружаем в папку src/css
 });
 
-gulp.task('css-main-nano', gulp.parallel('sass'), function () {
-  return gulp.src([ // Выбираем файл для минификации
-    'css/main.css',
-  ])
-    .pipe(cssnano()) // Сжимаем
-    .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
-    .pipe(gulp.dest('css')); // Выгружаем в папку src/css
-});
-
-gulp.task('watch', gulp.parallel('browser-sync', 'css-libs', 'scripts'), function () {
+gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function () {
   gulp.watch('sass/**/*.sass', ['sass']); // Наблюдение за sass файлами в папке sass
   gulp.watch('*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
-  gulp.watch('html/**/*.html', browserSync.reload); // Наблюдение за всеми HTML файлами
   gulp.watch('js/**/*.js', browserSync.reload);   // Наблюдение за JS файлами в папке js
 });
 
@@ -94,11 +85,11 @@ gulp.task('img', function () {
     .pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
 });
 
-gulp.task('build', gulp.parallel('clean', 'img', 'sass', 'scripts'), function () {
+gulp.task('build', ['clean', 'img', 'sass', 'scripts'], function () {
 
   var buildCss = gulp.src([ // Переносим библиотеки в продакшен
-    'css/main.css',
-    'css/libs.min.css'
+    'main.css',
+    'libs.min.css'
   ])
     .pipe(gulp.dest('dist/css'));
 
@@ -110,9 +101,6 @@ gulp.task('build', gulp.parallel('clean', 'img', 'sass', 'scripts'), function ()
 
   var buildHtml = gulp.src('*.html') // Переносим HTML в продакшен
     .pipe(gulp.dest('dist'));
-  
-  var buildHtml = gulp.src('html/**/*.html') // Переносим HTML в продакшен
-    .pipe(gulp.dest('dist/html'));
 
 });
 
@@ -120,4 +108,4 @@ gulp.task('clear', function (callback) {
   return cache.clearAll();
 });
 
-gulp.task('default', gulp.parallel('watch'));
+gulp.task('default', ['watch']);
